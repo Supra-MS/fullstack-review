952d11e2dae051be06d3a0da3ae48adb4cc1c342
Supra-MS <suprabharathi.m.s@gmail.com>
2020-07-21 01:58:50 -0700

    Complete save with unique records

--- database/index.js
+++ database/index.js
@@ -2,15 +2,50 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
-});
+  id: {
+    type: Number,
+    required: true,
+    unique: true,
+    index: true
+  },
+  repoName: {
+    type: String
+  },
+  owner: {
+    type: String
+  },
+  html_url: {
+    type: String
+  }
+},
+  {
+    timestamp: true
+  }
+);
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (repoDetails) => {
+  // InsertMany()
+
+  repoDetails.forEach(repo => {
+    Repo.findOne({id: repo.id}, (err, res) => {
+      if (err) {
+        console.log('Error in finding a single id: ', err);
+      } else {
+        if (res) {
+          console.log('Record already exists!!');
+        } else {
+          Repo.create({
+            id: repo.id,
+            repoName: repo.name,
+            owner: repo.owner.login,
+            html_url: repo.html_url
+          });
+        }
+      }
+    })
+  })
 }
 
 module.exports.save = save;
